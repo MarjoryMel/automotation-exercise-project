f2efa3473976ad82d9db75f055609a3f0cbbdb2e
MarjoryMel <marjorymel48@gmail.com>
2024-06-17 16:47:12 -0300

    tests on navbar

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,4 +1,59 @@
 Cypress.Commands.add('checkLogo', () => {
     cy.get('img[src="/static/images/home/logo.png"]')
-      .should('be.visible')
+        .should('be.visible')
   })
+
+Cypress.Commands.add('checkNavBar' ,() =>{
+    cy.get('.shop-menu > .nav')
+        .should('be.visible')
+})
+
+Cypress.Commands.add('checkNavbarButtons', () => {
+    for (let i = 1; i <= 8; i++) {
+        cy.get(`.shop-menu > .nav > :nth-child(${i}) > a`)
+            .should('be.visible')
+    }
+})
+
+Cypress.Commands.add('clickOnTheNavbarHome', () => {
+    cy.get('.shop-menu > .nav > :nth-child(1) > a')
+    .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarProducts', () => {
+    cy.get('.shop-menu > .nav > :nth-child(2) > a')
+    .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarCart', () => {
+    cy.get('.shop-menu > .nav > :nth-child(3) > a')
+    .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarLogin', () => {
+    cy.get('.shop-menu > .nav > :nth-child(4) > a')
+    .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarTestCases', () => {
+    cy.get('.shop-menu > .nav > :nth-child(5) > a')
+    .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarApi', () => {
+    cy.get('.shop-menu > .nav > :nth-child(6) > a')
+    .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarVideo', () => {
+    cy.get('.shop-menu > .nav > :nth-child(7) > a')
+        .invoke('removeAttr', 'target')
+        .click()
+})
+
+Cypress.Commands.add('clickOnTheNavbarContact', () => {
+    cy.get('.shop-menu > .nav > :nth-child(8) > a')
+    .click()
+})
+
+
